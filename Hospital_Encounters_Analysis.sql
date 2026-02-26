b0406116e69ace8a7c4124cee59e68c9e94fdf4b
MariiaYarmolenko <5188793@gmail.com>
2026-02-26 13:11:20 +0200

    Update Hospital_Encounters_Analysis.sql

--- Hospital_Encounters_Analysis.sql
+++ Hospital_Encounters_Analysis.sql
@@ -1,3 +1,8 @@
+# SQL (MySQL)
+To overcome technical limitations and accurately calculate time intervals, the following commands were used:
+
+Disabling Safe Update Mode
+
 set SQL_MODE = '';
 
 alter table hospital_db.encounters add column Start_DateTime datetime;
